c310d382db63bae8172525b2a0fb70c3bdb46c06
Gustavo Ramos <103971510+Gustaramos@users.noreply.github.com>
2022-08-24 17:43:58 -0300

    Update conta.py

--- conta/conta.py
+++ conta/conta.py
@@ -1,20 +1,36 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Criando objetos... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
     def transferencia(self, destino, valor):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
